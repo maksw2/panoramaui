1ab761ad572bc509d9a291c9201caab2e516367b
maksw2 <maksw@maksw.pl>
2024-08-18 16:36:16 +0200

    add

---
+++ script.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const button1 = document.getElementById('button1');
+  const button2 = document.getElementById('button2');
+
+  button1.addEventListener('click', () => {
+    console.log('pressed button 1');
+  });
+
+  button2.addEventListener('click', () => {
+    console.log('pressed button 2');
+  });
+  
+  button3.addEventListener('click', () => {
+    console.log('pressed button 3');
+  });
+  
+  button4.addEventListener('click', () => {
+    console.log('pressed button 4');
+  });
+  
+  button5.addEventListener('click', () => {
+    console.log('pressed button 5');
+  });
+  
+  button6.addEventListener('click', () => {
+    console.log('pressed button 6');
+  });
+});
